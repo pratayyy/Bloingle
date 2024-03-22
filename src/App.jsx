57f97f4879456b7189105e913d41c6d3a1d9fcde
pratayyy <pratayroy_official@yahoo.com>
2024-03-22 13:41:01 +0530

    user authentication form added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Authentication from "./pages/Authentication";
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-    </>
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route path="signup" element={<Authentication type="sign-up" />} />
+        <Route path="login" element={<Authentication type="login" />} />
+      </Route>
+    </Routes>
   );
 };
 
