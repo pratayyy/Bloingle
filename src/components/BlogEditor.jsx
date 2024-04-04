86df21f9fb97d6200f19fece39c76046e84a18de
pratayyy <pratayroy_official@yahoo.com>
2024-04-04 23:53:52 +0530

    blog title texture added

--- src/components/BlogEditor.jsx
+++ src/components/BlogEditor.jsx
@@ -31,6 +31,17 @@ const BlogEditor = () => {
     }
   };
 
+  const handleTitleKeyDown = (e) => {
+    if (e.keyCode === 13) e.preventDefault();
+  };
+
+  const handleTitleChange = (e) => {
+    const input = e.target;
+
+    input.style.height = "auto";
+    input.style.height = input.scrollHeight + "px";
+  };
+
   return (
     <>
       <nav className="navbar">
@@ -61,6 +72,13 @@ const BlogEditor = () => {
                 />
               </label>
             </div>
+
+            <textarea
+              placeholder="Blog Title"
+              className="text-4xl font-medium w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40"
+              onKeyDown={handleTitleKeyDown}
+              onChange={handleTitleChange}
+            ></textarea>
           </div>
         </section>
       </AnimationWrapper>
