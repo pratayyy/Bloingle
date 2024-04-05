cdcdd747d23fc4480d3fa6803012459b0910055b
pratayyy <pratayroy_official@yahoo.com>
2024-04-05 20:07:23 +0530

    publish form added

--- src/components/BlogEditor.jsx
+++ src/components/BlogEditor.jsx
@@ -24,7 +24,7 @@ const BlogEditor = () => {
     setTextEditor(
       new EditorJS({
         holder: "textEditor",
-        data: "",
+        data: content,
         tools: tools,
         placeholder: "Let's write an awesome story",
       })
@@ -122,6 +122,7 @@ const BlogEditor = () => {
             </div>
 
             <textarea
+              defaultValue={title}
               placeholder="Blog Title"
               className="text-4xl font-medium w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40"
               onKeyDown={handleTitleKeyDown}
--- src/components/PublishForm.jsx
+++ src/components/PublishForm.jsx
@@ -1,8 +1,134 @@
+import { useContext } from "react";
+import { Toaster, toast } from "react-hot-toast";
+
+import AnimationWrapper from "../common/AnimationWrapper";
+import { EditorContext } from "../pages/Editor";
+import Tag from "./Tag";
+
 const PublishForm = () => {
+  const characterLimit = 200;
+  const tagLimit = 10;
+
+  const {
+    blog,
+    blog: { title, banner, tags, description },
+    setEditorState,
+    setBlog,
+  } = useContext(EditorContext);
+
+  const handleCloseEvent = () => {
+    setEditorState("editor");
+  };
+
+  const handleBlogTitleChange = (e) => {
+    const input = e.target.value;
+    setBlog({ ...blog, title: input });
+  };
+
+  const handleBlogDescriptionChange = (e) => {
+    const input = e.target.value;
+    setBlog({ ...blog, description: input });
+  };
+
+  const handleTitleKeyDown = (e) => {
+    if (e.keyCode == 13) e.preventDefault();
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.keyCode == 13 || e.keyCode == 188) {
+      e.preventDefault();
+
+      const tag = e.target.value;
+
+      if (tags.length < tagLimit) {
+        if (!tags.includes(tag) && tag.length)
+          setBlog({ ...blog, tags: [...tags, tag] });
+      } else {
+        toast.error(`You can add max ${tagLimit} tags`);
+      }
+
+      e.target.value = "";
+    }
+  };
+
   return (
-    <>
-      <h1>Publish Form</h1>
-    </>
+    <AnimationWrapper>
+      <section className="w-screen min-h-screen grid items-center lg:grid-cols-2 py-16 lg:gap-4">
+        <Toaster />
+
+        <button
+          className="w-12 h-12 absolute right-[5vw] z-10 top-[5%] lg:top-[10%]"
+          onClick={handleCloseEvent}
+        >
+          <i className="fi fi-br-cross"></i>
+        </button>
+
+        <div className="max-w-[550px] center">
+          <p className="text-dark-grey mb-1">Preview</p>
+
+          <div className="w-full aspect-video rounded-lg overflow-hidden bg-grey mt-4">
+            <img src={banner} />
+          </div>
+
+          <h1 className="text-4xl font-medium mt-2 leading-tight line-clamp-2">
+            {title}
+          </h1>
+
+          <p className="font-gelasio line-clamp-2 text-xl leading-7 mt-4">
+            {description}
+          </p>
+        </div>
+
+        <div className="border-grey lg:border-1 lg:pl-8">
+          <p className="text-dark-grey mb-2 mt-9">Blog Title</p>
+          <input
+            type="text"
+            placeholder="Blog Title"
+            defaultValue={title}
+            className="input-box pl-4"
+            onChange={handleBlogTitleChange}
+          />
+
+          <p className="text-dark-grey mb-2 mt-9">
+            Short description about your blog
+          </p>
+          <textarea
+            maxLength={characterLimit}
+            defaultValue={description}
+            className="h-40 resize-none leading-7 input-box pl-4"
+            onChange={handleBlogDescriptionChange}
+            onKeyDown={handleTitleKeyDown}
+          ></textarea>
+
+          <p className="mt-1 text-dark-grey text-sm text-right">
+            {characterLimit - description.length} characters left
+          </p>
+
+          <p className="text-dark-grey mb-2 mt-9">
+            Topics - ( Helps in searching and ranking your blog post )
+          </p>
+
+          <div className="relative input-box pl-2 py-2 pb-4">
+            <input
+              type="text"
+              placeholder="Topics"
+              className="sticky input-box bg-white top-0 left-0 pl-4 mb-3 focus:bg-white"
+              onKeyDown={handleKeyDown}
+            />
+
+            {tags.map((tag, i) => {
+              return <Tag tag={tag} key={i} />;
+            })}
+          </div>
+
+          <p className="mt-1 mb-4 text-dark-grey text-right">
+            {tagLimit - tags.length} tags left
+          </p>
+
+          <button className="btn-dark px-8">Publish</button>
+        </div>
+      </section>
+    </AnimationWrapper>
   );
 };
 
