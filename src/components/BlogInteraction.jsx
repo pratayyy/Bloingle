2d256e517de9360020d156320dd1262689f527ed
pratayyy <pratayroy_official@yahoo.com>
2024-04-20 03:18:20 +0530

    blog interaction component added

--- src/components/BlogInteraction.jsx
+++ src/components/BlogInteraction.jsx
@@ -1,8 +1,69 @@
+import { useContext } from "react";
+import { BlogContext } from "../pages/Blog";
+import { Link } from "react-router-dom";
+import { UserContext } from "../App";
+
 const BlogInteraction = () => {
+  const {
+    blog: {
+      slug,
+      title,
+      activity,
+      activity: { totalLikes, totalComments },
+      author: {
+        personalInfo: { username: authorUsername },
+      },
+      setBlog,
+    },
+  } = useContext(BlogContext);
+
+  const {
+    user: {
+      data: { username },
+    },
+  } = useContext(UserContext);
+
   return (
-    <div>
-      <h1>Blog interaction</h1>
-    </div>
+    <>
+      <hr className="border-grey my-2" />
+
+      <div className="flex gap-6 justify-between">
+        <div className="flex gap-3 items-center">
+          <button className="w-10 h-10 rounded-full flex items-center justify-center bg-grey/80">
+            <i className="fi fi-rr-heart"></i>
+          </button>
+
+          <p className="text-xl text-dark-grey">{totalLikes}</p>
+
+          <button className="w-10 h-10 rounded-full flex items-center justify-center bg-grey/80">
+            <i className="fi fi-rr-comment-dots"></i>
+          </button>
+
+          <p className="text-xl text-dark-grey">{totalComments}</p>
+        </div>
+
+        <div className="flex gap-6 items-center">
+          {username === authorUsername ? (
+            <Link
+              to={`/editor/${slug}`}
+              className="underline hover:text-purple"
+            >
+              Edit
+            </Link>
+          ) : (
+            ""
+          )}
+
+          <Link
+            to={`https://www.twitter.com/intent/tweet?text=Read ${title}&url=${location.href}`}
+          >
+            <i className="fi fi-brands-twitter text-xl hover:text-twitter"></i>
+          </Link>
+        </div>
+      </div>
+
+      <hr className="border-grey my-2" />
+    </>
   );
 };
 
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 import AnimationWrapper from "../common/AnimationWrapper";
@@ -16,6 +16,8 @@ export const blogStructure = {
   banner: "",
   publishedAt: "",
 };
+
+export const BlogContext = createContext({});
 
 const Blog = () => {
   const { slug } = useParams();
@@ -59,36 +61,42 @@ const Blog = () => {
       {loading ? (
         <Loader />
       ) : (
-        <div className="max-w-[900px] center py-10 max-lg:px-[5vw] ">
-          <img src={banner} className="aspect-video" />
+        <BlogContext.Provider value={{ blog, setBlog }}>
+          <div className="max-w-[900px] center py-10 max-lg:px-[5vw] ">
+            <img src={banner} className="aspect-video" />
 
-          <div className="mt-12">
-            <h2>{title}</h2>
+            <div className="mt-12">
+              <h2>{title}</h2>
 
-            <div className="flex max-sm:flex-col justify-between my-8">
-              <div className="flex gap-5 items-start">
-                <img src={photo} className="w-12 h-12 rounded-full" />
+              <div className="flex max-sm:flex-col justify-between my-8">
+                <div className="flex gap-5 items-start">
+                  <img src={photo} className="w-12 h-12 rounded-full" />
 
-                <p>
-                  {name}
-                  <br />
-                  <Link
-                    to={`/user/${authorUsername}`}
-                    className="text-dark-grey"
-                  >
-                    @{authorUsername}
-                  </Link>
+                  <p>
+                    {name}
+                    <br />
+                    <Link
+                      to={`/user/${authorUsername}`}
+                      className="text-dark-grey"
+                    >
+                      @{authorUsername}
+                    </Link>
+                  </p>
+                </div>
+
+                <p className="text-dark-grey opacity-75 max-sm:mt-6 max-sm:ml-12 max-sm:pl-5">
+                  Published on {getDay(publishedAt)}
                 </p>
               </div>
-
-              <p className="text-dark-grey opacity-75 max-sm:mt-6 max-sm:ml-12 max-sm:pl-5">
-                Published on {getDay(publishedAt)}
-              </p>
             </div>
-          </div>
 
-          <BlogInteraction />
-        </div>
+            <BlogInteraction />
+
+            {/* Blog content  */}
+
+            <BlogInteraction />
+          </div>
+        </BlogContext.Provider>
       )}
     </AnimationWrapper>
   );
