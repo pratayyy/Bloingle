ffda029d7a8732c71e012bb9c54beba8f819fff6
pratayyy <pratayroy_official@yahoo.com>
2024-04-20 02:47:01 +0530

    blog header done

---
+++ src/components/BlogInteraction.jsx
@@ -0,0 +1,9 @@
+const BlogInteraction = () => {
+  return (
+    <div>
+      <h1>Blog interaction</h1>
+    </div>
+  );
+};
+
+export default BlogInteraction;
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,6 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+
+import AnimationWrapper from "../common/AnimationWrapper";
+import Loader from "../components/Loader";
+import { getDay } from "../common/Date";
+import BlogInteraction from "../components/BlogInteraction";
 
 export const blogStructure = {
   title: "",
@@ -16,6 +21,7 @@ const Blog = () => {
   const { slug } = useParams();
 
   const [blog, setBlog] = useState(blogStructure);
+  const [loading, setLoading] = useState(true);
 
   const {
     title,
@@ -23,7 +29,7 @@ const Blog = () => {
     content,
     tags,
     author: {
-      personalInfo: { name, usernmae, photo },
+      personalInfo: { name, username: authorUsername, photo },
     },
     banner,
     publishedAt,
@@ -35,22 +41,56 @@ const Blog = () => {
         method: "GET",
         url: import.meta.env.VITE_SERVER_DOMAIN + `/api/v1/blogs/${slug}`,
       });
-      console.log(res.data.blog);
+
       setBlog(res.data.blog);
+      setLoading(false);
     } catch (err) {
       console.log(err);
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     fetchBlog();
-    console.log(blog);
   }, []);
 
   return (
-    <div>
-      <h1>This is the blog page of {title}</h1>
-    </div>
+    <AnimationWrapper>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="max-w-[900px] center py-10 max-lg:px-[5vw] ">
+          <img src={banner} className="aspect-video" />
+
+          <div className="mt-12">
+            <h2>{title}</h2>
+
+            <div className="flex max-sm:flex-col justify-between my-8">
+              <div className="flex gap-5 items-start">
+                <img src={photo} className="w-12 h-12 rounded-full" />
+
+                <p>
+                  {name}
+                  <br />
+                  <Link
+                    to={`/user/${authorUsername}`}
+                    className="text-dark-grey"
+                  >
+                    @{authorUsername}
+                  </Link>
+                </p>
+              </div>
+
+              <p className="text-dark-grey opacity-75 max-sm:mt-6 max-sm:ml-12 max-sm:pl-5">
+                Published on {getDay(publishedAt)}
+              </p>
+            </div>
+          </div>
+
+          <BlogInteraction />
+        </div>
+      )}
+    </AnimationWrapper>
   );
 };
 
