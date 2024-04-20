bd85f44b050fc1ebd7b20989ac72bcb31ea6d0e7
pratayyy <pratayroy_official@yahoo.com>
2024-04-21 04:04:10 +0530

    like functionality added

--- src/components/BlogInteraction.jsx
+++ src/components/BlogInteraction.jsx
@@ -1,36 +1,109 @@
-import { useContext } from "react";
-import { BlogContext } from "../pages/Blog";
+import { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { Toaster, toast } from "react-hot-toast";
+
+import { BlogContext } from "../pages/Blog";
 import { UserContext } from "../App";
+import axios from "axios";
 
 const BlogInteraction = () => {
   const {
+    blog,
     blog: {
+      _id,
       slug,
       title,
-      activity,
-      activity: { totalLikes, totalComments },
       author: {
         personalInfo: { username: authorUsername },
       },
-      setBlog,
+    },
+    setBlog,
+    isLikedByUser,
+    setIsLikedByUser,
+  } = useContext(BlogContext);
+
+  let {
+    blog: {
+      activity,
+      activity: { totalLikes, totalComments },
     },
   } = useContext(BlogContext);
 
   const {
-    user: {
-      data: { username },
-    },
+    user: { data, token },
   } = useContext(UserContext);
+
+  const getUserLikeStatus = async () => {
+    try {
+      const res = await axios({
+        method: "GET",
+        url:
+          import.meta.env.VITE_SERVER_DOMAIN +
+          `/api/v1/blogs/get-user-like-status/${_id}`,
+        headers: {
+          "Authorization": `Bearer ${token}`,
+        },
+      });
+
+      setIsLikedByUser(Boolean(res.data.result));
+    } catch (err) {
+      toast.error(err.response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    if (token) getUserLikeStatus();
+  }, []);
+
+  const handleLike = async () => {
+    if (token) {
+      setIsLikedByUser((preVal) => !preVal);
+
+      !isLikedByUser ? totalLikes++ : totalLikes--;
+
+      setBlog({ ...blog, activity: { ...activity, totalLikes } });
+
+      try {
+        const res = await axios({
+          method: "PATCH",
+          url: import.meta.env.VITE_SERVER_DOMAIN + `/api/v1/blogs/${_id}`,
+          data: { isLikedByUser },
+          headers: {
+            "Authorization": `Bearer ${token}`,
+          },
+        });
+
+        res.data.notification !== null
+          ? toast.success("Liked! 😊")
+          : toast("Disliked!", { icon: "🥺" });
+      } catch (err) {
+        toast.error(err.response.data.message);
+      }
+    } else {
+      toast.error("Please log in to like this blog!");
+    }
+  };
 
   return (
     <>
+      <Toaster />
+
       <hr className="border-grey my-2" />
 
       <div className="flex gap-6 justify-between">
         <div className="flex gap-3 items-center">
-          <button className="w-10 h-10 rounded-full flex items-center justify-center bg-grey/80">
-            <i className="fi fi-rr-heart"></i>
+          <button
+            onClick={handleLike}
+            className={
+              "w-10 h-10 rounded-full flex items-center justify-center " +
+              (isLikedByUser ? "bg-red/20 text-red" : "bg-grey/80")
+            }
+          >
+            <i
+              className={
+                "fi " + (isLikedByUser ? "fi-sr-heart" : "fi-rr-heart")
+              }
+            ></i>
           </button>
 
           <p className="text-xl text-dark-grey">{totalLikes}</p>
@@ -43,7 +116,7 @@ const BlogInteraction = () => {
         </div>
 
         <div className="flex gap-6 items-center">
-          {username === authorUsername ? (
+          {data?.username === authorUsername ? (
             <Link
               to={`/editor/${slug}`}
               className="underline hover:text-purple"
