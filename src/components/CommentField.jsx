4052e46808614c0df6f3d0b9fb1133875987eae4
pratayyy <pratayroy_official@yahoo.com>
2024-04-22 22:45:04 +0530

    creating comments from ui

--- src/components/CommentField.jsx
+++ src/components/CommentField.jsx
@@ -1,10 +1,83 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import axios from "axios";
+
+import { UserContext } from "../App";
+import { BlogContext } from "../pages/Blog";
 
 const CommentField = ({ action }) => {
+  const {
+    user: { token },
+  } = useContext(UserContext);
+
+  const {
+    blog,
+    blog: {
+      _id: blogId,
+      author: { _id: blogAuthor },
+      activity,
+      activity: { totalComments, totalParentComments },
+      comments,
+    },
+    setBlog,
+    setTotalParentCommentsLoaded,
+  } = useContext(BlogContext);
+
   const [comment, setComment] = useState("");
+
+  const handleComment = async () => {
+    try {
+      const { data } = await axios({
+        method: "POST",
+        url: import.meta.env.VITE_SERVER_DOMAIN + "/api/v1/comments",
+        data: {
+          blogId,
+          blogAuthor,
+          content: comment,
+        },
+        headers: {
+          "Authorization": `Bearer ${token}`,
+        },
+      });
+
+      console.log(data.comment);
+
+      if (data.status === "success")
+        toast.success(`${action[0].toUpperCase() + action.slice(1)} added`);
+
+      setComment("");
+
+      let newCommentArr;
+
+      data.comment.childrenLevel = 0;
+
+      newCommentArr = [data];
+
+      const parentCommentIncrementValue = 1;
+
+      setBlog({
+        ...blog,
+        comments: { ...comments, results: newCommentArr },
+        activity: {
+          ...activity,
+          totalComments: totalComments + 1,
+          totalParentComments:
+            totalParentComments + parentCommentIncrementValue,
+        },
+      });
+
+      setTotalParentCommentsLoaded(
+        (previousValue) => previousValue + parentCommentIncrementValue
+      );
+    } catch (err) {
+      toast.error(err.response.data.message);
+    }
+  };
 
   return (
     <>
+      <Toaster />
+
       <textarea
         value={comment}
         placeholder="Leave a comment..."
@@ -12,7 +85,9 @@ const CommentField = ({ action }) => {
         className="input-box pl-5 placeholder:text-dark-grey resize-none h-[150px] overflow-auto"
       ></textarea>
 
-      <button className="btn-dark mt-5 px-10 ">{action}</button>
+      <button onClick={handleComment} className="btn-dark mt-5 px-10 ">
+        {action}
+      </button>
     </>
   );
 };
